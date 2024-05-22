467366074c00e44153c3e214cbe0019f64fdbf89
Leslie2186 <leslie.moraud@gmail.com>
2024-05-22 17:31:27 +0200

    Challenge plugin upload ok

--- cypress/e2e/challenge4.cy.js
+++ cypress/e2e/challenge4.cy.js
@@ -1,5 +1,13 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+describe('Uploader et télécharger des images', () => {
+  beforeEach(() => {
+    cy.visit('https://imgur.com/');
+  });
+
+  it("Upload d'un fichier et téléchargement au format zip", () => {
+    cy.get('[href="/upload"]').click();
+    cy.wait(2000);
+    cy.get("#file-input").attachFile("../fixtures/chat.jpg");
+    cy.wait(2000);
+    cy.contains("Download post").click();
+  });
 })
